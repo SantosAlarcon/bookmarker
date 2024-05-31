aa1817d7e2fb31d4e0e96bbe4eb996a32946d4dd
Santos Alarcón <santosalarcon86@gmail.com>
2024-05-31 11:35:35 +0200

    fix: main page redirect to login if there is no session

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,30 +3,23 @@ import Header from "@/components/Header/Header";
 import BookmarksView from "@/components/BookmarksView/BookmarksView";
 import type { GetStaticProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useSession } from "@/app/utils/supabase/useSession";
+import type { Session } from "@supabase/supabase-js";
 
 type Props = () => {
     locale: string
 }
 
 function Home() {
-	/*const supabase: SupabaseClient = createClient();
-	const {
-		data: { session },
-	} = await supabase.auth.getSession();
+	const session: Session | null = useSession()
 
-    console.log("Renderizando la raiz de pages...")
-
-    useEffect(() => {
-        if (!session) {
-            redirect("/auth/login");
-        }
-    }, [])*/
+    // It only renders the main page if there is no session
+    if (!session) return null
 
 	return (
 		<div className={styles.main}>
 			<Header />
             <BookmarksView />
-            {/*<BookmarksView />*/}
 		</div>
 	);
 }
