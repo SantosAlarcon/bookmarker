57c79afd18bfeeeea1993c600b14f248fdce76e4
Santos Alarcón <santosalarcon86@gmail.com>
2024-10-24 12:59:53 +0200

    fix(style): profile size image in auth button

--- app/[lang]/profile/page.tsx
+++ app/[lang]/profile/page.tsx
@@ -7,131 +7,101 @@ import Link from "next/link";
 import { initTranslations } from "@/app/i18n";
 import { createClient } from "@/app/utils/supabase/server";
 
-async function ProfilePage(props: {params: Promise<{lang: string}>}) {
-    const params = await props.params;
-    const supabase = await createClient();
+async function ProfilePage(props: { params: Promise<{ lang: string }> }) {
+	const params = await props.params;
+	const supabase = await createClient();
 
-    const {
-        lang
-    } = params;
+	const { lang } = params;
 
-    const { t, i18n } = await initTranslations(lang, ["profile-page"]);
+	const { t, i18n } = await initTranslations(lang, ["profile-page"]);
 
-    // If there is no session, it redirects to the login page
-    const {data: {user}} = await supabase.auth.getUser();
+	// If there is no session, it redirects to the login page
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
 
-    const userMetadata: UserMetadata | undefined = user?.user_metadata;
+	const userMetadata: UserMetadata | undefined = user?.user_metadata;
 
-    return (
-            <section className={styles.profile__page__container}>
-                <h1 className={styles.profile__page__title}>{t("title")}</h1>
-                <div className={styles.profile__page__avatar}>
-                    <picture>
-                        <img
-                            className={styles.profile__page__avatar__img}
-                            alt={userMetadata?.full_name}
-                            src={userMetadata?.picture}
-                            loading="lazy"
-                        />
-                    </picture>
-                </div>
-                <div className={styles.profile__page__data__grid}>
-                    <span
-                        className={
-                            styles.profile__page__data__grid__field__name
-                        }
-                    >
-                        {t("username")}
-                    </span>
-                    <span
-                        className={
-                            styles.profile__page__data__grid__field__value
-                        }
-                    >
-                        {userMetadata?.full_name}
-                    </span>
-                    <span
-                        className={
-                            styles.profile__page__data__grid__field__name
-                        }
-                    >
-                        {t("email")}
-                    </span>
-                    <span
-                        className={
-                            styles.profile__page__data__grid__field__value
-                        }
-                    >
-                        {userMetadata?.email}
-                    </span>
-                    <span
-                        className={
-                            styles.profile__page__data__grid__field__name
-                        }
-                    >
-                        {t("verified-by-email")}
-                    </span>
-                    <span
-                        className={
-                            styles.profile__page__data__grid__field__value
-                        }
-                    >
-                        {userMetadata?.email_verified ? (
-                            <TrueIcon />
-                        ) : (
-                            <FalseIcon />
-                        )}
-                    </span>
-                    <span
-                        className={
-                            styles.profile__page__data__grid__field__name
-                        }
-                    >
-                        {t("creation-date")}
-                    </span>
-                    <span
-                        className={
-                            styles.profile__page__data__grid__field__value
-                        }
-                    >
-                        {new Date(user?.created_at!).toLocaleString(
-                            i18n.language,
-                            {
-                                day: "numeric",
-                                month: "long",
-                                year: "numeric",
-                            },
-                        )}
-                    </span>
-                    <span
-                        className={
-                            styles.profile__page__data__grid__field__name
-                        }
-                    >
-                        {t("last-login-date")}
-                    </span>
-                    <span
-                        className={
-                            styles.profile__page__data__grid__field__value
-                        }
-                    >
-                        {new Date(
-                            user?.last_sign_in_at!,
-                        ).toLocaleString(i18n.language, {
-                            day: "numeric",
-                            month: "long",
-                            year: "numeric",
-                            hour: "numeric",
-                            minute: "numeric",
-                            hour12: false,
-                        })}
-                    </span>
-                </div>
-                <Link className={styles.profile__page__back__button} href="/">
-                    {t("back-to-home-button")}
-                </Link>
-            </section>
-    );
+	return (
+		<section className={styles.profile__page__container}>
+			<h1 className={styles.profile__page__title}>{t("title")}</h1>
+			<div className={styles.profile__page__avatar}>
+				<picture>
+					<img
+						className={styles.profile__page__avatar__img}
+						alt=""
+						src={userMetadata?.picture}
+						loading="lazy"
+						width="64"
+						height="64"
+					/>
+				</picture>
+			</div>
+			<div className={styles.profile__page__data__grid}>
+				<span className={styles.profile__page__data__grid__field__name}>
+					{t("username")}
+				</span>
+				<span
+					className={styles.profile__page__data__grid__field__value}
+				>
+					{userMetadata?.full_name}
+				</span>
+				<span className={styles.profile__page__data__grid__field__name}>
+					{t("email")}
+				</span>
+				<span
+					className={styles.profile__page__data__grid__field__value}
+				>
+					{userMetadata?.email}
+				</span>
+				<span className={styles.profile__page__data__grid__field__name}>
+					{t("verified-by-email")}
+				</span>
+				<span
+					className={styles.profile__page__data__grid__field__value}
+				>
+					{userMetadata?.email_verified ? (
+						<TrueIcon />
+					) : (
+						<FalseIcon />
+					)}
+				</span>
+				<span className={styles.profile__page__data__grid__field__name}>
+					{t("creation-date")}
+				</span>
+				<span
+					className={styles.profile__page__data__grid__field__value}
+				>
+					{new Date(user?.created_at!).toLocaleString(i18n.language, {
+						day: "numeric",
+						month: "long",
+						year: "numeric",
+					})}
+				</span>
+				<span className={styles.profile__page__data__grid__field__name}>
+					{t("last-login-date")}
+				</span>
+				<span
+					className={styles.profile__page__data__grid__field__value}
+				>
+					{new Date(user?.last_sign_in_at!).toLocaleString(
+						i18n.language,
+						{
+							day: "numeric",
+							month: "long",
+							year: "numeric",
+							hour: "numeric",
+							minute: "numeric",
+							hour12: false,
+						},
+					)}
+				</span>
+			</div>
+			<Link className={styles.profile__page__back__button} href="/">
+				{t("back-to-home-button")}
+			</Link>
+		</section>
+	);
 }
 
 export default ProfilePage;
--- app/components/Buttons/AuthButton/AuthButton.tsx
+++ app/components/Buttons/AuthButton/AuthButton.tsx
@@ -5,9 +5,7 @@ import { authStore } from "@/store/authStore";
 import tooltipStyles from "@/styles/tooltip.module.css";
 import type { Session } from "@supabase/auth-helpers-nextjs";
 import type { UserMetadata } from "@supabase/supabase-js";
-import type { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import { type SyntheticEvent, useEffect } from "react";
 import { Tooltip } from "react-tooltip";
 import styles from "./AuthButton.module.scss";
@@ -17,19 +15,8 @@ const AuthButton = () => {
     const session: Session | null = authStore.getState().session;
     const metadata: UserMetadata | null = authStore((state) => state.metadata);
 
-    const router: AppRouterInstance = useRouter();
-
     useEffect(() => {
         setAuth(session);
-        // const fetchSession = async () => {
-        //     const supabase = createClient();
-        //     const {data: {session}} = await supabase.auth.getSession();
-        //     setAuth(session);
-        // };
-        //
-        // if (!session) {
-        //     fetchSession();
-        // }
     }, []);
 
     const handleAuth = (event: SyntheticEvent) => {
@@ -37,10 +24,6 @@ const AuthButton = () => {
             // @ts-ignore
             handleUserContextMenu(event);
         } 
-
-        // else {
-        //     router.push("/auth/login");
-        // }
     };
 
     return (
@@ -67,12 +50,13 @@ const AuthButton = () => {
                                 src={metadata?.picture}
                                 width={36}
                                 height={36}
-                                alt="User avatar"
+                                alt=""
                                 fetchPriority="high"
+				className={styles.auth__button__img}
                             />
                         </picture>
                     ) : (
-                        <Image width={32} height={32} src={"/user.svg"} alt="User icon" />
+                        <Image className={styles.auth__button__img} width={36} height={36} src={"/user.svg"} alt="" />
                     )}
                 </button>
             </div>
