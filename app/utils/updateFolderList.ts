6e1bdd6800e3f5fe406c3f6f229a9ac7effeb17c
Santos Alarcón <santosalarcon86@gmail.com>
2024-10-29 20:55:06 +0100

    fix?: update list about type

--- app/utils/updateFolderList.ts
+++ app/utils/updateFolderList.ts
@@ -4,5 +4,6 @@ import { getAllFolders } from "./supabase/folders/getAllFolders";
 export const updateFolderList = async (userId: string) => {
     const setFolderList = folderStore.getState().setFolderList;
     const folderList = await getAllFolders(userId);
+    // @ts-ignore
     setFolderList(folderList);
 };
